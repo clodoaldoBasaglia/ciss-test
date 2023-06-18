3dfaa299cbbb492c03160d390dddce570fbf0ede
Clodoaldo Basaglia da Fonseca <clodoaldo.fonseca@db1.com.br>
2023-06-18 02:05:23 -0300

    Starting backend application

---
+++ ciss-test-backend/src/main/java/fonseca/basaglia/cisstestbackend/CissTestBackendApplication.java
@@ -0,0 +1,13 @@
+package fonseca.basaglia.cisstestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CissTestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CissTestBackendApplication.class, args);
+	}
+
+}
---
+++ ciss-test-backend/src/test/java/fonseca/basaglia/cisstestbackend/CissTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fonseca.basaglia.cisstestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CissTestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
